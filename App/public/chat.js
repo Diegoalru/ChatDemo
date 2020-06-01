899e69fbee761f53bb55ca9650a4f0496174e2f7
Diego Rubí Salas <diegoalru@gmail.com>
2020-06-01 03:10:49 -0600

    Cambios en el chat.js

--- App/public/chat.js
+++ App/public/chat.js
@@ -1,9 +1,10 @@
 
 $(function () {
-    //make connection
+
+    //Crear conexion 
     var socket = io.connect('http://localhost:3000')
 
-    //buttons and inputs
+    //Variables
     var message = $("#message")
     var username = $("#username")
     var send_message = $("#send_message")
@@ -11,29 +12,29 @@ $(function () {
     var chatroom = $("#chatroom")
     var feedback = $("#feedback")
 
-    //Emit message
+    //Envia mensaje
     send_message.click(function () {
         socket.emit('new_message', { message: message.val() })
     })
 
-    //Listen on new_message
+    //Escucha nuevos mensajes
     socket.on("new_message", (data) => {
         feedback.html('');
         message.val('');
         chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
     })
 
-    //Emit a username
+    //Cambia usuario
     send_username.click(function () {
         socket.emit('change_username', { username: username.val() })
     })
 
-    //Emit typing
+    //Emite evento de estar escribiendo
     message.bind("keypress", () => {
         socket.emit('typing')
     })
 
-    //Listen on typing
+    //Esta a la escucha del evento
     socket.on('typing', (data) => {
         feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
     })
